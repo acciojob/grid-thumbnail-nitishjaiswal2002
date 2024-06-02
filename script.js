63d30801751b9d966bbeb55a296039af1e4c2a2f
Nitish Jaiswal <141999440+nitishjaiswal2002@users.noreply.github.com>
2024-06-02 20:18:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+  const thumbnails = document.querySelectorAll('.thumbnail');
+
+  thumbnails.forEach(thumbnail => {
+    thumbnail.addEventListener('click', function() {
+      if (thumbnail.classList.contains('expanded')) {
+        thumbnail.classList.remove('expanded');
+        thumbnails.forEach(thumb => thumb.classList.remove('hidden'));
+      } else {
+        thumbnails.forEach(thumb => thumb.classList.add('hidden'));
+        thumbnail.classList.add('expanded');
+        thumbnail.classList.remove('hidden');
+      }
+    });
+  });
+});
